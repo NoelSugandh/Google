3553c1e9281a96618d7679bf65792bb85e59fbb4
Noel Sugandh <44922476+NoelSugandh@users.noreply.github.com>
2019-02-04 19:44:17 +0530

    Add files via upload

--- google.php
+++ google.php
@@ -13,9 +13,8 @@
     <body>
       <nav id="header">
         <ul>
-          <li><a href="">You</a></li>
           <li><a href="">Gmail</a></li>
-          <li><a href="">Images</a></li>
+          <li class="Images"><a href="">Images</a></li>
           <li class="Apps"><a href=""></a></li>
           <li class="Signin"><a href=""><b>Sign In</b></a></li>
         </ul>
@@ -27,7 +26,8 @@
         <input type="submit" value="Google Search">
           <input type="submit" value="I'm Feeling Lucky">
       </form>
-       <p><small>Google Offered in: <a href="">Telugu</a></small></p>
+    <small>Google Offered in: <a href="">Telugu</a>  <a href="">Hindi</a>
+        <a href="">Tamil</a> <a href="">Malayalam</a>  <a href="">Odissa</a>  <a href="">Gujarathi</a> <a href="">Bengali</a> <a href="">Kannada</a></small>
      </div>
        <footer class="top">
          <nav>
@@ -47,7 +47,6 @@
                  <li><a href="">Privacy</a></li>
                  <li><a href="">Terms</a></li>
                  <li><a href="">Settings</a></li>
-                 <li><a href="">Use Google.com</a></li>
                </ul>
               </nav>
        </footer>
